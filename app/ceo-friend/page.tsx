3d4ba0bdf33c57f25109db71a1634c540a9351be
Soojeong <soojlee0106@naver.com>
2025-04-15 15:06:26 +0900

    fix: 타이틀 수정

--- app/ceo-friend/page.tsx
+++ app/ceo-friend/page.tsx
@@ -1,10 +1,10 @@
 import CeoFriend from "../_pages/CeoFriend";
 
 export const metadata = {
-  title: "갖추 | 횡령, 남의 일이 아닙니다.",
+  title: "횡령, 남의 일이 아닙니다.",
   description: "대표님을 위한 횡령 탐지 솔루션",
 };
 
 export default function Page() {
-  return <CeoFriend></CeoFriend>
+  return <CeoFriend></CeoFriend>;
 }
--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -1,7 +1,7 @@
 import Employee from "../_pages/Employee";
 
 export const metadata = {
-  title: "갖추 | 자금 안전 솔루션",
+  title: "자금 안전 솔루션",
   description: "우리 회사에 도입할 자금 안전 솔루션",
 };
 
